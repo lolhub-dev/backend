0a5fbd234a919b234d5737fe500bd98f426f64c3
Daniel Gusenburger <danielgusenburger@online.de>
2019-12-09 20:27:54 +0100

    updated name in build.sbt

--- build.sbt
+++ build.sbt
@@ -1,4 +1,4 @@
-name := "akka-quickstart-scala"
+name := "lolhub"
 
 version := "1.0"
 
--- mongo.sh
+++ mongo.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/bash
-docker run --name lolhub_db --rm -v /data/db:/data/db -d mongo 
+docker run --name lolhub_db --rm -v /data/db:/data/db -p 27017:27017 -d mongo 
