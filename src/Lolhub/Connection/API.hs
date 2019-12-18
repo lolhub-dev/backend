a448a85941b076a51225ca8245f7e3bf285dcee2
Daniel Tabellion <daniel.tabellion@dfki.de>
2019-12-18 16:36:03 +0100

    added user to schema -> stub resolver

--- src/Lolhub/Connection/API.hs
+++ src/Lolhub/Connection/API.hs
@@ -1,4 +1,6 @@
+{-# LANGUAGE DataKinds             #-}
 {-# LANGUAGE DeriveGeneric         #-}
+{-# LANGUAGE DerivingStrategies    #-}
 {-# LANGUAGE FlexibleContexts      #-}
 {-# LANGUAGE FlexibleInstances     #-}
 {-# LANGUAGE MultiParamTypeClasses #-}
@@ -10,6 +12,7 @@
 
 module Lolhub.Connection.API
   ( gqlApi
+  , EVENT
   )
 where
 
@@ -20,23 +23,58 @@ import           Data.Morpheus.Document         ( importGQLDocumentWithNamespace
                                                 )
 import           Data.Morpheus.Types            ( GQLRootResolver(..)
                                                 , Undefined(..)
+                                                , ResolveQ
+                                                , ResolveM
+                                                , ResolveS
+                                                , IORes
+                                                , Event(..)
+                                                , constRes
+                                                , liftEither
                                                 )
 import           Data.Text                      ( Text )
 
 importGQLDocumentWithNamespace "src/schema.gql"
 
-rootResolver :: GQLRootResolver IO () Query Undefined Undefined
-rootResolver = GQLRootResolver { queryResolver        = Query { queryDeity }
-                               , mutationResolver     = Undefined
-                               , subscriptionResolver = Undefined
-                               }
- where
-  queryDeity QueryDeityArgs { queryDeityArgsName } = pure Deity { deityName
-                                                                , deityPower
-                                                                }
-   where
-    deityName _ = pure "Morpheus"
-    deityPower _ = pure (Just "Shapeshifting")
+data Channel = USER | ADDRESS
+  deriving (Show, Eq, Ord)
+
+newtype Content = Content { contentID :: Int  }
+
+type EVENT = (Event Channel Content)
 
 gqlApi :: B.ByteString -> IO B.ByteString
-gqlApi = interpreter rootResolver
+gqlApi = interpreter gqlRoot
+
+gqlRoot :: GQLRootResolver IO EVENT Query Undefined Undefined
+gqlRoot = GQLRootResolver { queryResolver
+                          , mutationResolver
+                          , subscriptionResolver
+                          }
+ where
+  queryResolver =
+    Query { queryUser = resolveUser, queryHelloWorld = resolveHelloWorld }
+  -------------------------------------------------------------
+  mutationResolver     = Undefined
+  subscriptionResolver = Undefined
+
+
+----- QUERY RESOLVERS -----
+
+resolveUser :: () -> ResolveQ EVENT IO User
+resolveUser _args = liftEither (getDBUser (Content 2))
+
+resolveHelloWorld :: () -> IORes EVENT Text
+resolveHelloWorld = constRes "helloWorld"
+
+getDBUser :: Content -> IO (Either String (User (IORes EVENT)))
+getDBUser _ = do
+  Person { name, email } <- dbPerson
+  pure $ Right User { userName = constRes name, userEmail = constRes email }
+
+data Person = Person {
+  name :: Text,
+  email :: Text
+}
+
+dbPerson :: IO Person
+dbPerson = pure Person { name = "George", email = "George@email.com" }
