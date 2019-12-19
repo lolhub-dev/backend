a3c7848f08ae212482293a5a439da2313b408c3c
Daniel Tabellion <daniel.tabellion@dfki.de>
2019-12-19 02:27:17 +0100

    worked on UserApi

--- app/Main.hs
+++ app/Main.hs
@@ -1,13 +1,13 @@
 {-# LANGUAGE OverloadedStrings #-}
+
 module Main where
 
-
-import           Lolhub.Connection.Graphql.UserApi
-                                                ( userApi )
+import           Lolhub.Connection.Graphql.UserApi (userApi)
 import           Control.Monad.IO.Class
 import           Web.Scotty
 
 main :: IO ()
-main = scotty 3000 $ do
+main = scotty 3000
+  $ do
     post "/user" $ raw =<< (liftIO . userApi =<< body)
-    -- post "/gamemodes" $ raw =<< (liftIO . gamemodeApi =<< body)
+-- post "/gamemodes" $ raw =<< (liftIO . gamemodeApi =<< body)
--- src/Lolhub/Connection/Graphql/UserApi.hs
+++ src/Lolhub/Connection/Graphql/UserApi.hs
@@ -1,47 +1,33 @@
-{-# LANGUAGE DataKinds             #-}
-{-# LANGUAGE DeriveGeneric         #-}
-{-# LANGUAGE DerivingStrategies    #-}
-{-# LANGUAGE FlexibleContexts      #-}
-{-# LANGUAGE FlexibleInstances     #-}
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE DerivingStrategies #-}
+{-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE FlexibleInstances #-}
 {-# LANGUAGE MultiParamTypeClasses #-}
-{-# LANGUAGE NamedFieldPuns        #-}
-{-# LANGUAGE OverloadedStrings     #-}
-{-# LANGUAGE ScopedTypeVariables   #-}
-{-# LANGUAGE TemplateHaskell       #-}
-{-# LANGUAGE TypeFamilies          #-}
+{-# LANGUAGE NamedFieldPuns #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE TemplateHaskell #-}
+{-# LANGUAGE TypeFamilies #-}
 
-module Lolhub.Connection.Graphql.UserApi
-  ( userApi
-  , USEREVENT
-  )
-where
+module Lolhub.Connection.Graphql.UserApi (userApi, USEREVENT) where
 
-import           Control.Monad.Trans            ( lift )
-import qualified Data.ByteString.Lazy.Char8    as B
-
-import           Data.Morpheus                  ( interpreter )
-import           Data.Morpheus.Document         ( importGQLDocumentWithNamespace
-                                                )
-import           Data.Morpheus.Types            ( GQLRootResolver(..)
-                                                , Undefined(..)
-                                                , ResolveQ
-                                                , ResolveM
-                                                , ResolveS
-                                                , IORes
-                                                , IOMutRes
-                                                , Event(..)
-                                                , constRes
-                                                , liftEither
-                                                )
-import           Data.Text                      ( Text )
+import           Control.Monad.Trans (lift)
+import qualified Data.ByteString.Lazy.Char8 as B
+import           Data.Morpheus (interpreter)
+import           Data.Morpheus.Document (importGQLDocumentWithNamespace)
+import           Data.Morpheus.Types (GQLRootResolver(..), Undefined(..)
+                                    , ResolveQ, ResolveM, ResolveS, IORes
+                                    , IOMutRes, Event(..), constRes, liftEither)
+import           Data.Text (Text)
 
 importGQLDocumentWithNamespace "src/Lolhub/Connection/Graphql/UserApi.gql"
 
-
-data Channel = USER | ADDRESS
+data Channel = USER
+             | ADDRESS
   deriving (Show, Eq, Ord)
 
-newtype Content = Content { contentID :: Int  }
+newtype Content = Content { contentID :: Int }
 
 type USEREVENT = (Event Channel Content)
 
@@ -49,46 +35,53 @@ userApi :: B.ByteString -> IO B.ByteString
 userApi = interpreter userGqlRoot
 
 userGqlRoot :: GQLRootResolver IO USEREVENT Query Mutation Undefined
-userGqlRoot = GQLRootResolver { queryResolver
-                              , mutationResolver
-                              , subscriptionResolver
-                              }
- where
-  queryResolver =
-    Query { queryLogin = loginUser, queryHelloWorld = resolveHelloWorld }
-  -------------------------------------------------------------
-  mutationResolver     = Mutation { mutationRegister = registerUser }
-  subscriptionResolver = Undefined
+userGqlRoot =
+  GQLRootResolver { queryResolver, mutationResolver, subscriptionResolver }
+  where
+    queryResolver =
+      Query { queryLogin = loginUser, queryHelloWorld = resolveHelloWorld }
 
+    -------------------------------------------------------------
+    mutationResolver = Mutation { mutationRegister = registerUser }
+
+    subscriptionResolver = Undefined
 
 ----- QUERY RESOLVERS -----
-
-loginUser :: QueryLoginArgs -> ResolveQ USEREVENT IO User
+loginUser :: QueryLoginArgs -> ResolveQ USEREVENT IO UnverifiedUser
 loginUser _args = liftEither (getDBUser (Content 2))
 
 resolveHelloWorld :: () -> IORes USEREVENT Text
 resolveHelloWorld = constRes "helloWorld"
 
 ----- MUTATION RESOLVERS
-registerUser :: MutationRegisterArgs -> ResolveM USEREVENT IO User
+registerUser :: MutationRegisterArgs -> ResolveM USEREVENT IO UnverifiedUser
 registerUser _args = lift setDBUser
 
-
-
-getDBUser :: Content -> IO (Either String (User (IORes USEREVENT)))
+getDBUser :: Content -> IO (Either String (UnverifiedUser (IORes USEREVENT)))
 getDBUser _ = do
-  Person { name, email } <- dbPerson
-  return $ Right User { userName = constRes name, userEmail = constRes email }
+  UnverifiedPerson { name, surname, email } <- dbPerson
+  return
+    $ Right
+      UnverifiedUser { unverifiedUserName = constRes name
+                     , unverifiedUserEmail = constRes email
+                     , unverifiedUserSurname = constRes surname
+                     }
 
-setDBUser :: IO (User (IOMutRes USEREVENT))
+setDBUser :: IO (UnverifiedUser (IOMutRes USEREVENT))
 setDBUser = do
-  Person { name, email } <- dbPerson
-  return User { userName = constRes name, userEmail = constRes email }
+  UnverifiedPerson { name, surname, email } <- dbPerson
+  return
+    UnverifiedUser { unverifiedUserName = constRes name
+                   , unverifiedUserEmail = constRes email
+                   , unverifiedUserSurname = constRes surname
+                   }
 
-data Person = Person {
-  name :: Text,
-  email :: Text
-}
+dbPerson :: IO UnverifiedPerson
+dbPerson = return
+  UnverifiedPerson { name = "George"
+                   , surname = "Hotz"
+                   , email = "George@email.com"
+                   }
 
-dbPerson :: IO Person
-dbPerson = return Person { name = "George", email = "George@email.com" }
+data UnverifiedPerson =
+  UnverifiedPerson { name :: Text, surname :: Text, email :: Text }
